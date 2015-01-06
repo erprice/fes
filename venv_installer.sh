3f62a6059bd8f5262488efb35f8b2d3582eeabdf
Evan Price <evan.price@bronto.com>
2015-01-05 23:12:08 -0500

    add command line param to start up fes

--- venv_installer.sh
+++ venv_installer.sh
@@ -1,9 +1,14 @@
 # This script installs all requirements in a virtual environment
 # Virtual environment is located at ~/.virtualenvs, the default
 # location for virtualenvwrapper.
+#
+# If the --startup flag is included, it will start up fes
 
 #create virtualenv location
-mkdir $HOME/.virtualenvs
+DIR="$HOME/.virtualenvs"
+if [ ! -d "$DIR" ]; then
+     mkdir $HOME/.virtualenvs
+fi
 cd $HOME/.virtualenvs
 
 #create and activate fes virtualenv
@@ -13,3 +18,8 @@ source $HOME/.virtualenvs/fes/bin/activate
 #install fes requirements
 cd $HOME/fes
 pip install -r requirements.txt
+
+#optionally start up fes
+if [ "$1" = "--startup" ]; then
+    python $HOME/fes/src/rest_view.py &
+fi
